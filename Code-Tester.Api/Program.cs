d6582d7604011da0d018737d9fc221accd5e0c3b
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2025-02-28 17:34:02 +0200

    feat: added docs for api endpoints

--- Code-Tester.Api/Program.cs
+++ Code-Tester.Api/Program.cs
@@ -2,6 +2,8 @@ using Code_Tester.Api.Services.CodeTester;
 using Code_Tester.Api.Services.CodeTester.Builders;
 using Code_Tester.Api.Services.CodeTester.Dependency;
 using Code_Tester.Api.Services.CodeTester.Testers;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Code Tester API",
+        Description = "An ASP.NET Core Web Api for executing and testing code remotely"
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddTransient<PythonStrategy>();
 builder.Services.AddTransient<CPlusPlusStrategy>();
